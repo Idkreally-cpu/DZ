c3a4cb448a66b091fdfae8a7e9bdf3760c002d61
Idkreally-cpu <oaoalvlva@gmail.com>
2025-06-08 08:58:49 +0900

    Update ConsoleApplication1.cs

--- 7/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cs
+++ 7/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cs
@@ -1,64 +1,61 @@
 using System;
 
+class Thermometer
+{
+    private double _temperature;
+    public double Temperature
+    {
+        get => _temperature;
+        set
+        {
+            if (_temperature != value)
+            {
+                _temperature = value;
+                OnTemperatureChanged();
+            }
+        }
+    }
+
+    public event Action<double> TemperatureChanged;
+
+    protected virtual void OnTemperatureChanged()
+    {
+        TemperatureChanged?.Invoke(_temperature);
+    }
+}
+
+class AirConditioner
+{
+    public void ReactToTemperature(double temp)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"AirConditioner: Temperature changed to {temp}°C");
+        Console.ResetColor();
+    }
+}
+
+class WarningSystem
+{
+    public void ReactToTemperature(double temp)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"WarningSystem: Temperature changed to {temp}°C");
+        Console.ResetColor();
+    }
+}
+
 class Program
 {
-    static void RemoveFirst(ref int[] array, out int removedElement)
-    {
-        if (array.Length == 0)
-        {
-            removedElement = default;
-            return;
-        }
-
-        removedElement = array[0];
-        int[] newArray = new int[array.Length - 1];
-        Array.Copy(array, 1, newArray, 0, newArray.Length);
-        array = newArray;
-    }
-
-    static void RemoveLast(ref int[] array, out int removedElement)
-    {
-        if (array.Length == 0)
-        {
-            removedElement = default;
-            return;
-        }
-
-        removedElement = array[array.Length - 1];
-        int[] newArray = new int[array.Length - 1];
-        Array.Copy(array, 0, newArray, 0, newArray.Length);
-        array = newArray;
-    }
-
-    static void RemoveAt(ref int[] array, int index, out int removedElement)
-    {
-        if (index < 0 || index >= array.Length)
-        {
-            removedElement = default;
-            return;
-        }
-
-        removedElement = array[index];
-        int[] newArray = new int[array.Length - 1];
-        Array.Copy(array, 0, newArray, 0, index);
-        Array.Copy(array, index + 1, newArray, index, newArray.Length - index);
-        array = newArray;
-    }
-
     static void Main()
     {
-        int[] numbers = { 1, 2, 3, 4, 5 };
+        var thermometer = new Thermometer();
+        var ac = new AirConditioner();
+        var ws = new WarningSystem();
 
-        RemoveFirst(ref numbers, out int first);
-        Console.WriteLine($"Удален первый элемент: {first}");
-        Console.WriteLine($"Массив: [{string.Join(", ", numbers)}]");
+        thermometer.TemperatureChanged += ac.ReactToTemperature;
+        thermometer.TemperatureChanged += ws.ReactToTemperature;
 
-        RemoveLast(ref numbers, out int last);
-        Console.WriteLine($"Удален последний элемент: {last}");
-        Console.WriteLine($"Массив: [{string.Join(", ", numbers)}]");
-
-        RemoveAt(ref numbers, 1, out int atIndex);
-        Console.WriteLine($"Удален элемент по индексу 1: {atIndex}");
-        Console.WriteLine($"Массив: [{string.Join(", ", numbers)}]");
+        thermometer.Temperature = 25;
+        thermometer.Temperature = 30;
     }
 }
